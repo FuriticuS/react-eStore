f3e93e0b17e81fff7918e68240f0811a775589f8
Fur1ouS <Fur1ouS@ya.ru>
2026-03-20 21:01:01 +0300

    add time close and open, add timer, add clock

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,19 @@
+import {useEffect, useState} from "react";
+
 export function Header(){
   const date = new Date();
+  const [time, setTime] = useState(date);
+
+  const hourNow = date.getHours()
+  const openStore = 8
+  const closeStore = 18
+  const isOpen = hourNow >= openStore && hourNow < closeStore;
+
+  useEffect(() => {
+    const intervalTime = setInterval(() => setTime(date), 1000)
+    return () => clearInterval(intervalTime)
+  })
+
   return (
     <header>
       <h1>Electronics store</h1>
@@ -9,7 +23,13 @@ export function Header(){
         <li key='About Us'>About Us</li>
         <li key='Contacts'>Contacts</li>
       </ul>
-      <p>{date.toLocaleString()}</p>
+      <p>
+        {isOpen ?
+          `Store open! The store will close in ${closeStore - hourNow} hours.`
+          : `Store close! The store will open in ${hourNow - openStore} hours.`}
+
+        <span> | {time.toLocaleTimeString('ru-RU')} {time.toLocaleDateString('ru-RU')}</span>
+      </p>
     </header>
   )
 }
