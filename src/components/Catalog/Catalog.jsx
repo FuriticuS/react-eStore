a00d8c02e4a663a90fffe6500472e116281a52f8
Fur1ouS <Fur1ouS@ya.ru>
2026-03-24 03:05:50 +0300

    add condition

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,12 +1,12 @@
 import {Product} from "../Product/Product.jsx";
 import style from "./Catalog.module.css"
 
-export function Catalog(props) {
+export function Catalog({productData}) {
   return (
     <main className={style.catalog}>
       <ul className={style.products}>
-        {props.productData.map(products => (
-          <Product key={products.id} products={products}/>
+        {productData.map(products => (
+          !products.soldOut && <Product key={products.id} products={products}/>
         ))}
       </ul>
     </main>
