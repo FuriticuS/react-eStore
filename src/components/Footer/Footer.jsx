0a31c3460671ba2ea407fa2c7be5414dff8aae76
Fur1ouS <Fur1ouS@ya.ru>
2026-03-25 03:20:12 +0300

    add hover img on useState of name

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import {useState} from "react";
 export function Footer() {
   const [count, setCount] = useState(0)
   const [isOpen, setIsOpen] = useState(false)
+  const [itemImg, setItemImg] = useState(null)
 
   function toggle(){
     setIsOpen(prev => !prev)
@@ -18,11 +19,11 @@ export function Footer() {
           <h1>Vite + React = {count >= 3 ? 'LOVE' : ''}</h1>
 
           <div className={style.logoContainer}>
-            <img src="/vite.svg" className={`${style.logo} ${count>=1 ? style.active:""}`} alt="Vite logo"/>
+            <img src="/vite.svg" className={`${style.logo} ${count>=1 || itemImg === 'vite' ? style.active:""}`} alt="Vite logo" onMouseEnter={() => setItemImg('vite')} onMouseLeave={() => setItemImg(null)}/>
             <p>+</p>
-            <img src="/react.svg" className={`${style.logo} ${count>=2 ? style.active:""}`} alt="React logo"/>
+            <img src="/react.svg" className={`${style.logo} ${count>=2 || itemImg === 'react' ? style.active:""}`} alt="React logo" onMouseEnter={() => setItemImg('react')} onMouseLeave={() => setItemImg(null)}/>
             <p>=</p>
-            <img src="/love.svg" className={`${style.logo} ${count>=3 ? style.active:""}`} alt="Love logo"/>
+            <img src="/love.svg" className={`${style.logo} ${count>=3 || itemImg === 'love' ? style.active:""}`} alt="Love logo" onMouseEnter={() => setItemImg('love')} onMouseLeave={() => setItemImg(null)}/>
           </div>
 
           <hr/>
