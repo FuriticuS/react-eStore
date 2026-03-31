384299b842dd5cbc3be6828a786e11e32e4974b8
Fur1ouS <Fur1ouS@ya.ru>
2026-03-31 03:15:01 +0300

    add PropTypes library

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,6 @@
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
-export function Button({title, icon, style, disabled= false, children, onClick}) {
-  return(
-    <button onClick={onClick} className={`${styles.button} ${disabled ? styles.disabled :''}`} disabled={disabled} style={style}>
-      {icon && <img src={icon} alt=""/>}
-      {title || children}
-    </button>
-  )
-}
-
 Button.propTypes = {
   title: PropTypes.string,
   icon: PropTypes.string,
@@ -17,4 +8,13 @@ Button.propTypes = {
   disabled: PropTypes.bool,
   children: PropTypes.string,
   onClick: PropTypes.func,
+}
+
+export function Button({title, icon, style, disabled= false, children, onClick}) {
+  return(
+    <button onClick={onClick} className={`${styles.button} ${disabled ? styles.disabled :''}`} disabled={disabled} style={style}>
+      {icon && <img src={icon} alt=""/>}
+      {title || children}
+    </button>
+  )
 }
